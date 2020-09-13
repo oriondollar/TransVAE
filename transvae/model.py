aaff217ebefa04983b2c1886d1fdf412ecfc0995
Orion Dollar <oriondollar@Orions-MacBook-Pro-2.local>
2020-09-13 13:37:20 -0700

    model_updates

--- transvae/model.py
+++ transvae/model.py
@@ -534,6 +534,15 @@ class LayerNorm(nn.Module):
     "Construct a layernorm module (See citation for details)"
     def __init__(self, features, eps=1e-6):
         super(LayerNorm, self).__init__()
+        self.bn = nn.BatchNorm1d(features)
+
+    def forward(self, x):
+        return self.bn(x)
+
+class PrevLayerNorm(nn.Module):
+    "Construct a layernorm module (See citation for details)"
+    def __init__(self, features, eps=1e-6):
+        super(PrevLayerNorm, self).__init__()
         self.a_2 = nn.Parameter(torch.ones(features))
         self.b_2 = nn.Parameter(torch.zeros(features))
         self.eps = eps
