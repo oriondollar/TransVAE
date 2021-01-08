d2bae9b1c14d07b97db6d5494e12a748a3eb7d48
PRG Admin <prgadmin@Chemicals-MacBook-Pro.local>
2021-01-08 12:07:49 -0800

    scripts

---
+++ scripts/sample.py
@@ -0,0 +1,62 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+sys.path.append('{}/transvae'.format(os.getcwd()))
+import pickle
+import pkg_resources
+
+import numpy as np
+import pandas as pd
+
+from trans_models import TransVAE
+from rnn_models import RNN, RNNAttn
+from tvae_util import calc_entropy
+from parsers import sample_parser
+
+def sample(args):
+    # Load model
+    ckpt_fn = args.model_ckpt
+    if args.model == 'transvae':
+        vae = TransVAE(load_fn=ckpt_fn)
+    elif args.model == 'rnnattn':
+        vae = RNNAttn(build=False)
+    elif args.model == 'rnn':
+        vae = RNN(build=False)
+    vae.load(ckpt_fn)
+
+    # Calculate entropy depending on sampling mode
+    if args.sample_mode == 'rand':
+        sample_mode = 'rand'
+        sample_dims = None
+    else:
+        entropy_data = pd.read_csv(args.smiles).to_numpy()
+        _, mus, _ = vae.calc_mems(entropy_data)
+        vae_entropy = calc_entropy(mus)
+        entropy_idxs = np.where(np.array(vae_entropy) > args.entropy_cutoff)[0]
+        sample_dims = entropy_idxs
+        if args.sample_mode == 'high_entropy':
+            sample_mode = 'top_dims'
+        elif args.sample_mode == 'k_high_entropy':
+            sample_mode = 'k_dims'
+
+    # Generate samples
+    samples = []
+    n_gen = args.n_samples
+    while n_gen > 0:
+        current_samples = vae.sample(args.batch_size, sample_mode=sample_mode,
+                                     sample_dims=sample_dims, k=args.k)
+        samples.extend(current_samples)
+        n_gen -= len(current_samples)
+
+    samples = pd.DataFrame(samples, columns=['smile'])
+    if args.save_path is None:
+        os.makedirs('generated', exist_ok=True)
+        save_path = 'generated/{}_{}.csv'.format(vae.name, args.sample_mode)
+    else:
+        save_path = args.save_path
+    samples.to_csv(save_path, index=False)
+
+if __name__ == '__main__':
+    parser = sample_parser()
+    args = parser.parse_args()
+    sample(args)
